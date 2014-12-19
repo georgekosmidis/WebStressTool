3e366151ed81e44efd1541e8e5835092b839462a
George Kosmidis <georgekosmidis@outlook.com>
2014-12-19 13:54:13 +0200

    Initial Commit

---
+++ WebStressTest/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WebStressTest {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing ) {
+            if ( disposing && (components != null) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.numConThreads = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numTries = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numConThreads)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTries)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(12, 11);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(266, 20);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = "http://www.e-forologia.gr/cms/default.aspx";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 64);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(428, 290);
+            this.txtLog.TabIndex = 2;
+            // 
+            // numConThreads
+            // 
+            this.numConThreads.Location = new System.Drawing.Point(388, 38);
+            this.numConThreads.Name = "numConThreads";
+            this.numConThreads.Size = new System.Drawing.Size(52, 20);
+            this.numConThreads.TabIndex = 3;
+            this.numConThreads.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Threads";
+            // 
+            // numTries
+            // 
+            this.numTries.Location = new System.Drawing.Point(275, 37);
+            this.numTries.Name = "numTries";
+            this.numTries.Size = new System.Drawing.Size(52, 20);
+            this.numTries.TabIndex = 5;
+            this.numTries.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tries";
+            // 
+            // btnStop
+            // 
+            this.btnStop.ForeColor = System.Drawing.Color.Black;
+            this.btnStop.Location = new System.Drawing.Point(365, 10);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 361);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(77, 13);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "Κλήση 0 από 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 383);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numTries);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numConThreads);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtURL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Web Stress Tester";
+            ((System.ComponentModel.ISupportInitialize)(this.numConThreads)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTries)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.NumericUpDown numConThreads;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numTries;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ WebStressTest/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Deamons.UI;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace WebStressTest {
+    public partial class Form1 : Form {
+
+        private List<Thread> _threads;
+        double GI = 0;
+        double CI = 0;
+        bool StopThreads = false;
+
+        public Form1() {
+            InitializeComponent();
+            Logging.TextBoxStatus = this.txtLog;
+
+            _threads = new List<Thread>();
+        }
+
+        private void button1_Click( object sender, EventArgs e ) {
+            txtLog.Text = "";
+            StopThreads = false;
+            btnStop.Text = "Stop";
+            Logging.WriteStatus( "Starting Threads" );
+
+            for ( int i = 0; i < numConThreads.Value; i++ ) {
+                var thread = new Thread(
+                        () => {
+                            if ( StopThreads ) {
+                                i = (int)numConThreads.Value;
+                                return;
+                            }
+                            var t =  Thread.CurrentThread.Name;
+                            for ( int j=0; j < numTries.Value; j++ ) {
+                                var dtStart = DateTime.Now;
+                                try {
+                                    CallResource();
+                                }
+                                catch {
+                                    Invoke( (MethodInvoker)delegate {
+                                        Logging.WriteStatus( "*********Call " + j + " from thread " + t + " threw an exception!!!!!!!!!!" );
+                                    } );
+                                    continue;
+                                }
+                                GI += (DateTime.Now - dtStart).TotalMilliseconds;
+                                CI++;
+
+                                Invoke( (MethodInvoker)delegate {
+                                    Logging.WriteStatus( "Call " + j + " from thread " + t + " duration " + (DateTime.Now - dtStart).TotalMilliseconds );
+                                    lblStatus.Text = "Κλήση " + CI + " από " + (numConThreads.Value * numTries.Value);
+                                } );
+
+                                if ( StopThreads ) {
+                                    Invoke( (MethodInvoker)delegate {
+                                        Logging.WriteStatus( "Thread " + t + " stopped " );
+                                    } );
+                                    return;
+                                }
+                            }
+                            OnWorkComplete();
+                        }
+                );
+                thread.IsBackground = true;
+                thread.Name = string.Format( "{0}", i );
+
+
+                _threads.Add( thread );
+                thread.Start();
+            }
+
+        }
+        int OnWorkComplete_i = 0;
+        void OnWorkComplete() {
+
+            OnWorkComplete_i++;
+            if ( numConThreads.Value <= OnWorkComplete_i ) {
+                Invoke( (MethodInvoker)delegate {
+                    Logging.WriteStatus( "Avg Time: " + (GI / (double)(numConThreads.Value * numTries.Value)) );
+                    btnStop.Text = "Stop";
+                } );
+            }
+            //StopThreads = false;
+
+        }
+
+        void CallResource() {
+            var request = (HttpWebRequest)WebRequest.Create( txtURL.Text );
+            request.Method = WebRequestMethods.Http.Get;
+            request.ContentType = "text/html";
+            request.Timeout = 1000 * 60 * 60;
+
+            using ( var response = (HttpWebResponse)request.GetResponse() ) {
+                using ( var dataStream = response.GetResponseStream() ) {
+                    using ( StreamReader reader = new StreamReader( dataStream ) ) {
+                        var s = reader.ReadToEnd().ToString();
+                    }
+                }
+            }
+        }
+
+        private void btnStop_Click( object sender, EventArgs e ) {
+            StopThreads = true;
+            btnStop.Text = "Stoping...";
+            //Invoke( (MethodInvoker)delegate {
+            //    Logging.WriteStatus( "Avg Time: " + (GI / (double)(numConThreads.Value + numConThreads.Value)) );
+            //} );
+        }
+    }
+}
